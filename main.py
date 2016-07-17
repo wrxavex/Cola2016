db8c9d11f197f3295384f2bee103237411fb349c
wrxavex <wrxavex@gmail.com>
2016-07-17 21:55:28 +0800

    add label

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class ColaApp(App):
 
     def update_mcp3008_value(self, nap):
         read_mcp3008()
-        # ˚self.root.ids.mcp3008_value.text = mcp3008_value
+        # self.root.ids.mcp3008_value.text = mcp3008_value
 
     def on_start(self):
         Clock.schedule_interval(self.update_mcp3008_value, 1)
