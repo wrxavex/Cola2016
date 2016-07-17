9599f8cd767541ac3f0f9d70ebd1ee0de2641cd4
wrxavex <wrxavex@gmail.com>
2016-07-18 00:37:19 +0800

    layout change

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class ColaApp(App):
     def update_mcp3008_value(self, nap):
         values = read_mcp3008()
 
-        values[0] = (values(0) - 396) * (470.0 / 627.0) + 130.0
+        values[0] = (values[0] - 396) * (470.0 / 627.0) + 130.0
         pwm.set_pwm(0, 0, int(values[0]))  # servo..LR
 
         print('rotation value: %d' % values[0])
