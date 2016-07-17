d74004cdfaff1153765e7a34a9b52a3fa06e7bee
wrxavex <wrxavex@gmail.com>
2016-07-17 22:00:13 +0800

    change kv's label id

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class ColaApp(App):
 
     def update_mcp3008_value(self, nap):
         read_mcp3008()
-        self.root.ids.mcp3008_value.text = 'test'
+        self.root.ids.mcp.text = 'test'
 
     def on_start(self):
         Clock.schedule_interval(self.update_mcp3008_value, 1)
